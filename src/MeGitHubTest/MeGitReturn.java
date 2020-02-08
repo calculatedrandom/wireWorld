526bf92117cb9cb079dc16be61caa2b5ced047d9
WhatEver <What@Ever.com>
2020-02-08 17:10:07 +1100

    commit 4 from intellij

--- src/MeGitHubTest/MeGitReturn.java
+++ src/MeGitHubTest/MeGitReturn.java
@@ -3,5 +3,6 @@ package MeGitHubTest;
 class MeGitReturn {
     public static void main(String[] args) {
         System.out.println("Hello gitHub");
+        System.out.println("more shit added");
     }
 }
